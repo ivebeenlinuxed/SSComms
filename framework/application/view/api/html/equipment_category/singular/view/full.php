e4f9781a554b9b3a61358bf66ce14fc6d4beca6a
root <root@localhost.localdomain>
2015-08-15 20:33:42 +0000

    Added statistics to equipment categories

--- framework/application/view/api/html/equipment_category/singular/view/full.php
+++ framework/application/view/api/html/equipment_category/singular/view/full.php
@@ -4,7 +4,7 @@
 <div class="row">
 	<div class="col-md-4 col-xs-12">
 		<div class="panel panel-default">
-			<div class="panel-heading">Team Information</div>
+			<div class="panel-heading">Category Information</div>
 			<div class="panel-body">
 				<div class="form-group">
 					<label class="control-label">Name</label> <input
@@ -12,6 +12,42 @@
 						data-field="name" data-id="<?php echo $equipment_category->id ?>"
 						value="<?php echo $equipment_category->name ?>" />
 				</div>
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading">Team Breakdown</div>
+			<div class="panel-body">
+				<table class="table table-striped table-bordered">
+					<tr>
+						<th>Team</th>
+						<th>#</th>
+					</tr>
+					<?php
+					$db = \Model\Team::getDB();
+					$rows = $db->Exec(<<<EOF
+SELECT COUNT(*) AS number, team.name AS team_name
+FROM equipment
+LEFT JOIN equipment_category ON equipment.category = equipment_category.id
+LEFT JOIN equipment_checkout ON equipment.id = equipment_checkout.equipment
+LEFT JOIN person ON equipment_checkout.person = person.id
+LEFT JOIN team ON person.team = team.id
+WHERE equipment_checkout.checkin=0 AND equipment_category.id={$equipment_category->id} GROUP BY team.id ORDER BY number DESC
+EOF
+);
+					foreach ($rows as $row) {
+					?>
+					<tr>
+						<td><?php echo $row['team_name'] ?></td>
+						<td>
+						<?php
+						echo $row['number'];
+						?>
+						</td>
+					</tr>
+					<?php
+					}
+					?>
+				</table>
 			</div>
 		</div>
 	</div>
@@ -34,7 +70,21 @@
 					<tr>
 						<td><a href="/api/equipment/<?php echo $equipment->id ?>"><?php echo $equipment->id ?></a></td>
 						<td><?php echo $equipment->name ?></td>
-						<td><?php echo ($equipment->isInService())? "In Service" : "Out of Service" ?></td>
+						<td><?php
+						if (!$equipment->isInService()) {
+							echo "Out of Service";
+						} elseif ($equipment->isCheckedOut()) {
+							echo "Checked Out to ";
+							$e = $equipment->getEquipmentCheckouts();
+							$co = $e[count($e)-1];
+							$person = $co->getPerson();
+							$team = $person->getTeam();
+							echo "<a href='/api/person/{$person->id}'>".$person->getName()."</a>";
+							echo " (<a href='/api/team/{$team->id}'>".$team->getName()."</a>)";
+						} else {
+							echo "Checked In";
+						}
+						?></td>
 					</tr>
 					<?php 
 					}

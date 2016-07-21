bb455648004596f5f4ba2b2d48a4ed127e0c3fef
Will Tinsdeall <will.tinsdeall@mercianlabels.com>
2016-07-21 21:16:17 +0100

    Fix for unexpected int

--- framework/application/controller/widget/livehelper/Asset_Change.php
+++ framework/application/controller/widget/livehelper/Asset_Change.php
@@ -12,7 +12,7 @@ class Asset_Change {
 		\Core\Router::loadView("live/helper/asset_change/out_person");
 	}
 	
-	public function change_asset_list(int $person_id) {
+	public function change_asset_list($person_id) {
 		$person = new \Model\Person($person_id);
 		\Core\Router::loadView("live/helper/asset_change/change_asset_list", array("person"=>$person));
 	}

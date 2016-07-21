e2842cb1135bd7ca2b84e29bcd707383dda68494
Will Tinsdeall <will.tinsdeall@mercianlabels.com>
2016-07-21 23:39:04 +0100

    Add user on the fly

--- framework/application/view/widget/person_searcher/template.php
+++ framework/application/view/widget/person_searcher/template.php
@@ -8,6 +8,45 @@
 </div>
 <div class="clearfix"></div>
 </template>
+
+<template id="person-searcher-new-templ">
+<div class="col-xs-12">
+<div class="form-group">
+	<label>First Name</label>
+	<input class="form-control" type="text" name="first_name" />
+</div>
+<div class="form-group">
+	<label>Last Name</label>
+	<input class="form-control" type="text" name="last_name" />
+</div>
+<div class="form-group">
+	<label>Phone Number</label>
+	<input class="form-control" type="text" name="phone_number" />
+</div>
+<div class="form-group">
+	<label>Call Sign</label>
+	<input class="form-control" type="text" name="call_sign" />
+</div>
+<div class="form-group">
+	<label>Team</label>
+	<select class="form-control" name="team">
+	<?php 
+	foreach(\Model\Team::getAll() as $team) {
+	?>
+	<option value="<?php echo $team->id ?>"><?php echo $team->getName() ?></option>
+	<?php 
+	}
+	?>
+	</select>
+</div>
+<div class="form-group">
+	<label>Barcode</label>
+	<input class="form-control" type="text" name="barcode" />
+</div>
+<button class="btn btn-success pull-right">Add Person</button>
+</div>
+</template>
+
 <template id="person-searcher-record-templ"> <div class="col-xs-3"><a href="#"
 		class="thumbnail">
 		<div class="media">
--- htdocs/js/widget/person_searcher.js
+++ htdocs/js/widget/person_searcher.js
@@ -16,6 +16,7 @@ class PersonSearcher {
 		this.input = parent.querySelector(".person-searcher-input");
 		this.container = parent.querySelector(".person-searcher-container");
 		this.template = imported_doc.querySelector("#person-searcher-record-templ");
+		this.new_form = imported_doc.querySelector("#person-searcher-new-templ");
 		
 		
 		this.input.addEventListener("keyup", (e) => {
@@ -45,6 +46,7 @@ class PersonSearcher {
 			response.json().then((json) => {
 				if (json.full_match) {
 					this.onselect(json.data[0]);
+					return;
 				}
 				
 				var el = document.importNode(this.template.content, true);
@@ -60,12 +62,54 @@ class PersonSearcher {
 					});
 					this.container.appendChild(node);
 				}
+				
+				var node = el.cloneNode(true);
+				node.person_data = row;
+				node.querySelector("h4").innerHTML = "[Add Person] "+this.input.value;
+				node.querySelector("a").addEventListener("click", () => {
+					this.AddNew();
+				});
+				this.container.appendChild(node);
 			});
 			
 		});
 	}
 	
+	AddNew() {
+		var el = document.importNode(this.new_form.content, true);
+		this.container.innerHTML = "";
+		this.container.appendChild(el);
+		
+		var e = this.input.value.split(" ");
+		
+		this.container.querySelector("input[name='first_name']").value = e[0];
+		if (e.length > 1) {
+			this.container.querySelector("input[name='last_name']").value = e.slice(1).join(" ");
+		}
+		
+		this.container.querySelector("button").addEventListener("click", () => {
+			this.SaveNew();
+		});
+	}
 	
+	SaveNew() {
+		var fd = new FormData();
+		fd.append("first_name", this.container.querySelector("input[name='first_name']").value);
+		fd.append("last_name", this.container.querySelector("input[name='last_name']").value);
+		fd.append("phone_number", this.container.querySelector("input[name='phone_number']").value);
+		fd.append("call_sign", this.container.querySelector("input[name='call_sign']").value);
+		fd.append("team", this.container.querySelector("select[name='team']").value);
+		fd.append("wristband_id", this.container.querySelector("input[name='barcode']").value);
+		fetch("/api/person.json", {
+			credentials: "include",
+			body: fd,
+			method: "post"
+		}).then((response) => {
+			response.json().then((json) => {
+				this.onselect(json.data);
+			});
+		});
+	}
 	
 	
 }
